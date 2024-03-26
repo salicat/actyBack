2e52ad2886d145f3a5e3b4180d53645914d7fc84
KrlzCrtz <71334080+salicat@users.noreply.github.com>
2024-03-26 16:07:22 -0500

    new database connection

--- db/db_connection.py
+++ db/db_connection.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://carloscortes:1234@127.0.0.1:5432/postgres"
+DATABASE_URL = "postgresql://udc2so76e3ro74:p3000bbcc372fced8b5567649d05e6bf749df779bf7a79ff942dd0f1ad639c9a7@cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dem0gsbrobr5t8"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, 
