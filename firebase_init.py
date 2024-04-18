11333c67022516a6c15b8b6766dd1bf853393c34
KrlzCrtz <71334080+salicat@users.noreply.github.com>
2024-04-17 21:23:26 -0500

    reupload

--- firebase_init.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-def initialize_firebase():
-    # Path to your Firebase service account key
-    key_path = 'FireB_JsonKey/otp1-d9db4-firebase-adminsdk-x0mbp-c4d7507b59.json'
-    cred = credentials.Certificate(key_path)
-    firebase_admin.initialize_app(cred)
